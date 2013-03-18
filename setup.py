78c814e10ecde03e5685142fabe4f22a286c926f
Allen Li <darkfeline@abagofapples.com>
2013-03-17 23:59:39 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='dantalian',
-    version='0.3',
+    version='0.4',
     author='Allen Li',
     author_email='darkfeline@abagofapples.com',
     url='http://abagofapples.com/',
