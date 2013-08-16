2b5561fb1dcdded515ee78a236769b0c970a31d0
Allen Li <darkfeline@abagofapples.com>
2013-08-15 20:42:34 -0700

    Add unit tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -2,21 +2,28 @@ import unittest
 import tempfile
 import shutil
 import os
+import logging
 
 from dantalian import library
+
+logger = logging.getLogger(__name__)
 
 
 class TestLibraryMethods(unittest.TestCase):
 
     def setUp(self):
+        self._olddir = os.getcwd()
         self.root = tempfile.mkdtemp()
         r = os.path.join(self.root, 'library')
-        os.makedirs(os.path.join(r, 'A', 'D'))
-        os.makedirs(os.path.join(r, 'B'))
-        os.makedirs(os.path.join(r, 'C'))
-        os.mknod(os.path.join(r, 'a'))
+        os.mkdir(r)
+        os.chdir(r)
+        os.makedirs(os.path.join('A', 'D'))
+        os.makedirs('B')
+        os.makedirs('C')
+        os.mknod('a')
+        os.mknod('b')
+        os.link('b', os.path.join('C', 'b'))
         self.library = library.init_library(r)
-        self._olddir = os.getcwd()
         os.chdir(self.root)
 
     def tearDown(self):
@@ -24,12 +31,37 @@ class TestLibraryMethods(unittest.TestCase):
         os.chdir(self._olddir)
 
     def assertTagged(self, a, b):
-        """Assert `b` is tagged at `a`"""
-        self.assertTrue(os.path.exists(a))
         self.assertTrue(os.path.samefile(a, b))
+
+    def assertNotTagged(self, a, b):
+        if os.path.exists(a) and os.path.exists(b):
+            self.assertFalse(os.path.samefile(a, b))
+        elif not os.path.exists(a) and not os.path.exists(b):
+            self.assertTrue(False)
+
+    def assertSameTags(self, a, b):
+        self.assertEqual(a, b)
+        self.assertEqual(set(a), set(b))
 
     def test_tag(self):
         l = self.library
         os.chdir('library')
         l.tag('a', '/A')
         self.assertTagged(os.path.join('A', 'a'), 'a')
+        l.tag('a', '/A/D')
+        self.assertTagged(os.path.join('A', 'D', 'a'), 'a')
+
+    def test_untag(self):
+        l = self.library
+        os.chdir('library')
+        self.assertTagged(os.path.join('C', 'b'), 'b')
+        l.untag('b', '/C')
+        self.assertNotTagged(os.path.join('C', 'b'), 'b')
+
+    def test_listtags(self):
+        l = self.library
+        os.chdir('library')
+        self.assertSameTags(l.listtags('a'), ['/'])
+        self.assertSameTags(l.listtags('b'), ['/', '/C'])
+        l.tag('b', '/A')
+        self.assertSameTags(l.listtags('b'), ['/', '/C', '/A'])
