c7f2032d22b28ac3f36f81db857e572029d3c816
Allen Li <darkfeline@abagofapples.com>
2013-08-29 18:47:05 -0700

    Remove autodoc, addd viewcode sphinx extension

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.viewcode']
 try:
     import sphinx.ext.ext_ctags
 except ImportError:
