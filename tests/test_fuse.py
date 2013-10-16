46f236adc96e258a92fc21617cb20e4d820abc9f
Allen Li <darkfeline@abagofapples.com>
2013-10-15 20:11:23 -0400

    Add framework for fuse test

--- tests/test_fuse.py
+++ tests/test_fuse.py
@@ -12,9 +12,10 @@ from dantalian.library import fs as library
 logger = logging.getLogger(__name__)
 
 
-class TestFUSE(unittest.TestCase):
+class TestMount(unittest.TestCase):
 
     def setUp(self):
+
         self._olddir = os.getcwd()
         self.root = tempfile.mkdtemp()
         r = os.path.join(self.root, 'library')
@@ -32,6 +33,56 @@ class TestFUSE(unittest.TestCase):
         os.mkdir(r)
 
     def tearDown(self):
+
+        shutil.rmtree(self.root)
+        os.chdir(self._olddir)
+
+    def umount(self, path):
+        return subprocess.call(['fusermount', '-u', path])
+
+    def test_mount(self):
+
+        l = self.library
+        self.proc = MountProcess(l, 'mnt', l.maketree())
+        self.proc.start()
+        time.sleep(0.5)
+
+        ret = self.umount('mnt')
+        self.assertEquals(ret, 0)
+        self.proc.join()
+
+
+class TestFUSE(unittest.TestCase):
+
+    def setUp(self):
+
+        self._olddir = os.getcwd()
+        self.root = tempfile.mkdtemp()
+        r = os.path.join(self.root, 'library')
+        os.mkdir(r)
+        os.chdir(r)
+        os.makedirs(os.path.join('A', 'D'))
+        os.makedirs('B')
+        os.makedirs('C')
+        os.mknod('a')
+        os.mknod('b')
+        os.link('b', os.path.join('C', 'b'))
+        self.library = library.init_library(r)
+        os.chdir(self.root)
+        r = os.path.join(self.root, 'mnt')
+        os.mkdir(r)
+        self.fusesock = library.Library.fusesock(r)
+
+        l = self.library
+        self.proc = MountProcess(l, 'mnt', l.maketree())
+        self.proc.start()
+        time.sleep(0.5)
+
+    def tearDown(self):
+
+        self.umount('mnt')
+        self.proc.join()
+
         shutil.rmtree(self.root)
         os.chdir(self._olddir)
 
@@ -51,14 +102,16 @@ class TestFUSE(unittest.TestCase):
         self.assertEqual(a, b)
         self.assertEqual(set(a), set(b))
 
-    def test_mount(self):
+    @unittest.skip('not working')
+    def test_mknode(self):
         l = self.library
-        proc = MountProcess(l, 'mnt', l.maketree())
-        proc.start()
-        time.sleep(1)
-        ret = self.umount('mnt')
-        self.assertEquals(ret, 0)
-        proc.join()
+        with open(self.fusesock, 'w') as f:
+            f.write('mknode E //C //B')
+            time.sleep(0.5)
+        self.assertTrue(os.path.isdir('E'))
+        self.assertNotIn('b', os.listdir('E'))
+        l.tag('b', '//B')
+        self.assertIn('b', os.listdir('E'))
 
 
 class MountProcess(multiprocessing.Process):
