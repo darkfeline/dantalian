2fafaad75a9ad5cf0819c8adaee5738f5720af2d
Allen Li <darkfeline@abagofapples.com>
2014-06-29 01:42:31 -0700

    Clean up setup.py metadata

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import setup, find_packages
 setup(
     name='dantalian',
     version='0.5',
-    author='Allen Li',
-    author_email='darkfeline@abagofapples.com',
-    url='http://abagofapples.com/',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     scripts=['src/bin/dantalian'],
+
+    author='Allen Li',
+    author_email='darkfeline@abagofapples.com',
+    description='',
+    license='MIT',
+    url='http://darkfeline.github.io/dantalian/',
 )
