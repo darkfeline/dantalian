06c94b7df95ff71a1135bc83dc5307bc52a5305d
Allen Li <darkfeline@abagofapples.com>
2014-07-29 15:34:40 -0700

    Fix Sphinx theme configuration

--- doc/conf.py
+++ doc/conf.py
@@ -104,8 +104,7 @@ except ImportError:
     html_theme = 'default'
 else:
     html_theme = "sphinx_rtd_theme"
-
-# Use ReadTheDoc theme
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -114,7 +113,6 @@ else:
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
