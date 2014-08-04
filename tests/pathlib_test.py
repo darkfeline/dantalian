f3bbd5ee362f66809ca911708a80cd39d1e8b78c
Allen Li <darkfeline@abagofapples.com>
2014-08-04 02:24:29 -0700

    Refactor pathlib unit tests

--- tests/pathlib_test.py
+++ tests/pathlib_test.py
@@ -4,12 +4,55 @@ import logging
 import tempfile
 import shutil
 
-from dantalian import pathlib as dpath
+from dantalian import pathlib
 
 logger = logging.getLogger(__name__)
 
 
-class TestPath(unittest.TestCase):
+class TestTagFunctions(unittest.TestCase):
+
+    def test_istag(self):
+        self.assertTrue(pathlib.istag('///tag'))
+        self.assertTrue(pathlib.istag('//tag'))
+        self.assertFalse(pathlib.istag('/tag'))
+        self.assertFalse(pathlib.istag('tag'))
+
+    def test_pathfromtag(self):
+
+        root = '/foo'
+        actual = pathlib.pathfromtag('//tag', root)
+        expected = os.path.join(root, 'tag')
+        self.assertEquals(actual, expected)
+
+        root = '/foo/bar'
+        actual = pathlib.pathfromtag('//tag1/tag2', root)
+        expected = os.path.join(root, 'tag1/tag2')
+        self.assertEquals(actual, expected)
+
+    def test_tagfrompath(self):
+
+        root = '/foo'
+        actual = pathlib.tagfrompath('/foo/tag', root)
+        expected = '//tag'
+        self.assertEquals(actual, expected)
+
+        root = os.getcwd()
+        actual = pathlib.tagfrompath('tag', root)
+        expected = '//tag'
+        self.assertEquals(actual, expected)
+
+        root = os.getcwd()
+        actual = pathlib.tagfrompath('./tag', root)
+        expected = '//tag'
+        self.assertEquals(actual, expected)
+
+        root = os.getcwd()
+        actual = pathlib.tagfrompath('./foo/../bar/tag', root)
+        expected = '//bar/tag'
+        self.assertEquals(actual, expected)
+
+
+class TestNameResolution(unittest.TestCase):
 
     def setUp(self):
         self._olddir = os.getcwd()
@@ -22,27 +65,7 @@ class TestPath(unittest.TestCase):
         shutil.rmtree(self.root)
         os.chdir(self._olddir)
 
-    def test_istag(self):
-        self.assertTrue(dpath.istag('//tag'))
-        self.assertTrue(dpath.istag('//tag/madoka'))
-        self.assertFalse(dpath.istag('/tag'))
-        self.assertFalse(dpath.istag('/tag/hi'))
-        self.assertFalse(dpath.istag('tag'))
-        self.assertFalse(dpath.istag('tag/bye'))
-
-    def test_pathfromtag(self):
-        r = os.getcwd()
-        o = os.path.join(r, 'tag')
-        self.assertEquals(dpath.pathfromtag('//tag', r), o)
-
-    def test_tagfrompath(self):
-        r = os.getcwd()
-        o = '//tag'
-        self.assertEquals(dpath.tagfrompath('tag', r), o)
-        self.assertEquals(dpath.tagfrompath(os.path.join(r, 'tag'), r), o)
-
     def test_resolve_name(self):
-        self.assertEquals(dpath.resolve_name(self.root, 'b'), 'b')
-        self.assertEquals(dpath.resolve_name(self.root, 'a'), 'a.1')
-        self.assertEquals(
-            dpath.resolve_name(self.root, 'a.mp3'), 'a.1.mp3')
+        self.assertEquals(pathlib.resolve_name(self.root, 'b'), 'b')
+        self.assertEquals(pathlib.resolve_name(self.root, 'a'), 'a.1')
+        self.assertEquals(pathlib.resolve_name(self.root, 'a.mp3'), 'a.1.mp3')
