df7eb0aa06fa34e3568725b0001c80a81dc19dc6
Allen Li <darkfeline@felesatra.moe>
2015-01-08 23:12:18 -0800

    Fix unit tests for dirlib.DirNode
    Removed DirNode subclass due to symlink change

--- tests/library_test.py
+++ tests/library_test.py
@@ -7,7 +7,6 @@ import os
 from . import testlib
 from dantalian import library
 from dantalian.library import baselib
-from dantalian.library import dirlib
 
 # pylint: disable=missing-docstring
 
@@ -19,55 +18,55 @@ class TestLibraryBaseParsing(testlib.TestCase):
     def test_parse_and(self):
         tree = library.parse_query(self._root, "AND A B C )")
         self.assertSameQuery(tree, baselib.AndNode(
-            [dirlib.DirNode("A"),
-             dirlib.DirNode("B"),
-             dirlib.DirNode("C")]))
+            [baselib.DirNode("A"),
+             baselib.DirNode("B"),
+             baselib.DirNode("C")]))
 
     def test_parse_or(self):
         tree = library.parse_query(self._root, "OR A B C )")
         self.assertSameQuery(tree, baselib.OrNode(
-            [dirlib.DirNode("A"),
-             dirlib.DirNode("B"),
-             dirlib.DirNode("C")]))
+            [baselib.DirNode("A"),
+             baselib.DirNode("B"),
+             baselib.DirNode("C")]))
 
     def test_parse_minus(self):
         tree = library.parse_query(self._root, "MINUS A B C )")
         self.assertSameQuery(tree, baselib.MinusNode(
-            [dirlib.DirNode("A"),
-             dirlib.DirNode("B"),
-             dirlib.DirNode("C")]))
+            [baselib.DirNode("A"),
+             baselib.DirNode("B"),
+             baselib.DirNode("C")]))
 
     def test_parse_and_escape(self):
         tree = library.parse_query(self._root,
                                    r"AND '\AND' '\\AND' '\\\AND' )")
         self.assertSameQuery(tree, baselib.AndNode(
-            [dirlib.DirNode(r'AND'),
-             dirlib.DirNode(r'\AND'),
-             dirlib.DirNode(r'\\AND')]))
+            [baselib.DirNode(r'AND'),
+             baselib.DirNode(r'\AND'),
+             baselib.DirNode(r'\\AND')]))
 
     def test_parse_paren_escape(self):
         tree = library.parse_query(self._root, r"AND A B \\) )")
         self.assertSameQuery(tree, baselib.AndNode(
-            [dirlib.DirNode(r'A'),
-             dirlib.DirNode(r'B'),
-             dirlib.DirNode(r')')]))
+            [baselib.DirNode(r'A'),
+             baselib.DirNode(r'B'),
+             baselib.DirNode(r')')]))
 
     def test_parse_and_or(self):
         tree = library.parse_query(self._root, "AND A B C OR spam eggs ) )")
         self.assertSameQuery(tree, baselib.AndNode(
-            [dirlib.DirNode("A"),
-             dirlib.DirNode("B"),
-             dirlib.DirNode("C"),
+            [baselib.DirNode("A"),
+             baselib.DirNode("B"),
+             baselib.DirNode("C"),
              baselib.OrNode(
-                 [dirlib.DirNode('spam'),
-                  dirlib.DirNode('eggs')])
+                 [baselib.DirNode('spam'),
+                  baselib.DirNode('eggs')])
             ]))
 
     def test_parse_tags(self):
         tree = library.parse_query(self._root, "AND A /B //C //D/E )")
         self.assertSameQuery(tree, baselib.AndNode(
-            [dirlib.DirNode("A"),
-             dirlib.DirNode("/B"),
-             dirlib.DirNode(os.path.join(self._root, 'C')),
-             dirlib.DirNode(os.path.join(self._root, 'D/E')),
+            [baselib.DirNode("A"),
+             baselib.DirNode("/B"),
+             baselib.DirNode(os.path.join(self._root, 'C')),
+             baselib.DirNode(os.path.join(self._root, 'D/E')),
             ]))
