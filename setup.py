b7755924a08e919c5e1c17ba53cd9104eed2a123
Allen Li <darkfeline@felesatra.moe>
2016-09-11 01:50:38 -0700

    Repackage

--- setup.py
+++ setup.py
@@ -2,7 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='dantalian',
-    version='1.0',
+    version='1.0.0',
+    description='File tagging with hard links',
+    long_description='',
+    keywords='',
+    url='http://darkfeline.github.io/dantalian/',
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3.5',
+    ],
+
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={
@@ -10,10 +25,4 @@ setup(
             'dantalian = dantalian.main:main',
         ],
     },
-
-    author='Allen Li',
-    author_email='darkfeline@felesatra.moe',
-    description='File tagging with hard links',
-    license='GPLv3',
-    url='http://darkfeline.github.io/dantalian/',
 )
